2c37ad64de0845266e9a1dff3b8d54de41c798df
srilakshmi364-0 <75250311+srilakshmi364-0@users.noreply.github.com>
2020-11-30 21:14:12 +0530

    Create sonyliv.py

---
+++ sonyliv.py
@@ -0,0 +1,12 @@
+import ffmpeg   
+import subprocess
+
+with open("tenali.csv", 'r') as file:
+    csv_reader = csv.DictReader(file, delimiter=',')
+    for row in csv_reader:
+        epno = row['Episode Number']
+        epname = row['Episode Name']
+        epurl = row['Video']
+
+cmd = f'youtube-dl -f "bestvideo[height=720]+bestaudio" -o "{epno}. {epname}" worstvideo+worstaudio --geo-bypass-country IN {epurl}'
+subprocess.call(cmd, shell=True)
