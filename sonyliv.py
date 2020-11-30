77b759492c082f5b61e22f4e9d86710f11b47ed5
srilakshmi364-0 <75250311+srilakshmi364-0@users.noreply.github.com>
2020-11-30 21:36:12 +0530

    Update sonyliv.py

--- sonyliv.py
+++ sonyliv.py
@@ -8,5 +8,6 @@ with open("tenali.csv", 'r') as file:
         epname = row['Episode Name']
         epurl = row['Video']
 
+print("Starting...")
 cmd = f'youtube-dl -f "bestvideo[height=720]+bestaudio" -o "{epno}. {epname}" worstvideo+worstaudio --geo-bypass-country IN {epurl}'
 subprocess.call(cmd, shell=True)
